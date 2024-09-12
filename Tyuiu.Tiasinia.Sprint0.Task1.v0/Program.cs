4869f1f035a53d892d931e37861bea543d1bb66b
Илья Тясин <turbopad7200@gmail.com>
2024-09-12 21:06:33 +0500

    Task1

---
+++ Tyuiu.Tiasinia.Sprint0.Task1.v0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tyuiu.Tiasinia.Sprint0.Task1.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World");
+            String name = "Илья";
+            int age = 21;
+            Console.WriteLine("Доброго времени суток, меня звать " + name + " Мне " + age + " год ");
+            Console.ReadKey(); 
+        }
+    }
+}
