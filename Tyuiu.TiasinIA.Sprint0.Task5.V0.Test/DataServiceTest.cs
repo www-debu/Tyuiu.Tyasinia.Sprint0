3adaa1f813993ac3e8b32b26df874d679ef8548c
Илья Тясин <turbopad7200@gmail.com>
2024-09-14 21:46:31 +0500

    Task5

---
+++ Tyuiu.TiasinIA.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,29 @@
+using Tyuiu.TiasinIA.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.TiasinIA.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionValild()
+        {
+            Assert.AreEqual(10, DataService.Addition(5, 5));
+        }
+        [TestMethod]
+        public void CheckedSubtractionValild()
+        {
+            Assert.AreEqual(5, DataService.Subtraction(10, 5));
+        }
+        [TestMethod]
+        public void CheckedMultiplicationValild()
+        {
+            Assert.AreEqual(50, DataService.Multiplication(10, 5));
+        }
+        [TestMethod]
+        public void CheckedDivisionValild()
+        {
+            Assert.AreEqual(3, DataService.Division(9, 3));
+        }
+    }
+}
