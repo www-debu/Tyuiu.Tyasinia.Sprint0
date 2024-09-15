217aa6e40ca40921e1dcf4c783342d87b1617280
Илья Тясин <turbopad7200@gmail.com>
2024-09-15 14:04:10 +0500

    Task6

---
+++ Tyuiu.TiasinIA.Sprint0.Task6.V0.Lib/DataService.cs
@@ -0,0 +1,40 @@
+﻿namespace Tyuiu.TiasinIA.Sprint0.Task6.V0.Lib
+{
+    public class DataService
+    {
+        public static object AdditionArray(int[] nubers)
+        {
+            var total = 0;
+            for (var i = 0; i < nubers.Length; i++)
+            {
+                total = total + nubers[i];
+            }
+            return total;
+        }
+        public static object SubstractionArray(int[] nubers)
+        {
+            var total = 0;
+            var index = 0;
+
+            while (index < nubers.Length)
+            {
+                total = total - nubers[index];
+                index++;
+            }
+            return total;
+        }
+        public static object MultiplicationArray(int[] nubers)
+        {
+            var total = 1;
+            int index = 0;
+            do
+            {
+                total = total * nubers[index];
+                index++;
+            }
+            while (index < nubers.Length);
+            return total;
+        }
+
+    }
+}
---
+++ Tyuiu.TiasinIA.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+using Tyuiu.TiasinIA.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.TiasinIA.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckAdditionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckSubtractionArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubstractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        [TestMethod]
+        public void CheckMultiplicationArrayValid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+        
+    }
+}
